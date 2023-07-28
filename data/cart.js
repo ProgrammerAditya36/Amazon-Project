15949f0f3a3147ebeffde11b7baf83a951e4fe64
Aditya Amit Kinjawadekar <programmeraditya36@gmail.com>
2023-07-28 19:20:30 +0530

    Delete products from cart when clicking Delete

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,13 @@
-const cart = [];
+let cart = [
+  {
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2,
+  },
+  {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1,
+  },
+];
 function addTOCart(productId, quantity) {
   let matchingItem;
   cart.forEach((cartItem) => {
@@ -14,4 +23,13 @@ function addTOCart(productId, quantity) {
       quantity,
     });
   }
+}
+function removeFromCart(productId) {
+  const newCart = [];
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+  cart = newCart;
 }
